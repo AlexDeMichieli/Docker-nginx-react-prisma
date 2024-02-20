e3571b0babce90250be7272c5c9561b6fd42d68e
alexdemichieli <alexdemichieli@github.com>
2024-02-19 21:29:24 -0800

    adding changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          444 <code>src/App.js</code> and save to reload.
+          fff <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
